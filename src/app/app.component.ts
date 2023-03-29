17b70161fcecbbcd464df07a69f319c100777c3f
Erick Neves <erick.h.neves@gmail.com>
2023-03-29 19:15:12 -0300

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  correios: Array<any> = [];
+
+  listaJson = {
+    "erro": false,
+    "mensagem": "DADOS ENCONTRADOS COM SUCESSO.",
+    "total": 1,
+    "dados": [
+      {
+        "uf": "SP",
+        "localidade": "S\u00e3o Paulo",
+        "locNoSem": "",
+        "locNu": "",
+        "localidadeSubordinada": "",
+        "logradouroDNEC": "Rua Planta da Sorte",
+        "logradouroTextoAdicional": "",
+        "logradouroTexto": "",
+        "bairro": "Vila Verde",
+        "baiNu": "",
+        "nomeUnidade": "",
+        "cep": "08235010",
+        "tipoCep": "2",
+        "numeroLocalidade": "",
+        "situacao": "",
+        "faixasCaixaPostal": [],
+        "faixasCep": []
+      }
+    ]
+  }
+
+  ngOnInit() {
+    this.returnarCorreios();
+  }
+
+  returnarCorreios() {
+    this.correios = this.listaJson.dados;
+  }
+
+
+  title = 'aula3';
+}
